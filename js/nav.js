e1c77f4237a8007d794c2fe008df1402575b482c
petechoi <petechoi10@gmail.com>
2016-07-20 13:53:08 +0900

    code refactoring

--- js/nav.js
+++ js/nav.js
@@ -1,40 +1,36 @@
 ---
 ---
-function ChildManager(){
+var animatedMenuBar = {
+	arry: [],
+	create: function (parent, menutag) {
+	    $(parent).children().each(function () {
+	        animatedMenuBar.arry.push($(this));
+	    });
+	    
+	    $(menutag).mouseover(function () {
+	        var distance = 27;
+	        for (var i = 1; i < 5; i++) {
+	            $(animatedMenuBar.arry[i]).animate({
+	                top: distance,
+	                opacity: '1'
+	            }, 200);
+	            distance += 22;
+	        }
+	    });
 	
+	    $(parent).mouseleave(function () {
+	        for (var i = 4; i >= 1; i--) {
+	            $(animatedMenuBar.arry[i]).animate({
+	                top: 27,
+	                opacity: '0'
+	            }, 200);
+	        }
+	    });
+	}
 }
 
-
 $(document).ready(function () {
-	
-    var animatedMenuBar = {
-        arry: [],
-        create: function (parent, menutag) {
-            $(parent).children().each(function () {
-                animatedMenuBar.arry.push($(this));
-            });
-            
-            $(menutag).mouseover(function () {
-                var distance = 27;
-                for (var i = 1; i < 5; i++) {
-                    $(animatedMenuBar.arry[i]).animate({
-                        top: distance,
-                        opacity: '1'
-                    }, 200);
-                    distance += 22;
-                }
-            });
-
-            $(parent).mouseleave(function () {
-                for (var i = 4; i >= 1; i--) {
-                    $(animatedMenuBar.arry[i]).animate({
-                        top: 27,
-                        opacity: '0'
-                    }, 200);
-                }
-            });
-        }
-    }
+     animatedMenuBar.create("div.nav","div.menu");
 });
 
 
